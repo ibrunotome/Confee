946b1521b5eb1a7e4df774d7220327c1e4579889
Bruno Tomé <ibrunotome@gmail.com>
2017-08-28 17:08:05 -0300

    Iniciando o domínio.

--- app/Domains/Users/Database/Migrations/CreateUsersTable.php
+++ app/Domains/Users/Database/Migrations/CreateUsersTable.php
@@ -1,35 +1,27 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
+namespace Confee\Domains\Users\Database\Migrations;
+
+
+use Confee\Support\Database\Migration;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Database\Migrations\Migration;
 
 class CreateUsersTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        $this->schema->create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('password');
+            $table->string('password')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
-        Schema::dropIfExists('users');
+        $this->schema->drop('users');
     }
 }
